b173e8c31ec2e74c55448c2fb636c9356d4f36c4
SevenWaysDP <sergej.hoffmann@dataport.de>
2026-09-10 10:17:57 +0200

    Align action button in SchuleDetailsView for improved layout

--- src/views/admin/organisationen/SchuleDetailsView.vue
+++ src/views/admin/organisationen/SchuleDetailsView.vue
@@ -93,11 +93,10 @@
                 </div>
               </v-col>
             </v-row>
-            <v-row>
+            <v-row class="justify-end">
               <v-col
                 cols="12"
-                col-md="4"
-                offset-md="8"
+                md="auto"
               >
                 <v-btn
                   class="primary"
